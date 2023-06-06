1e1d29e68e037d95c84042e5bec303a4acddf9ed
Bilal Ali <bilalal495@outlook.com>
2023-06-06 18:59:50 -0400

    Refactor getQuestions file into class

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
 const notionKey = process.env.NOTION_KEY;
-const databaseId = process.env.NOTION_DATABASE_ID;
+const questionDatabaseId = process.env.QUESTION_DATABASE_ID;
+const reviewDatabaseId = process.env.REVIEW_DATABASE_ID;
 
-export { notionKey, databaseId };
+export { notionKey, questionDatabaseId, reviewDatabaseId };
--- src/getQuestions.ts
+++ src/getQuestions.ts
@@ -1,36 +1,56 @@
-import { notionKey, databaseId } from "./config.js";
+import { QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints.js";
+import { notionKey } from "./config.js";
 import { Client, isFullPage } from "@notionhq/client";
 
-// Initialize notion client
-const client = new Client({ auth: notionKey });
+class GetQuestions {
+  private client: Client;
+  private databaseId: string;
+  questions: string[];
 
-console.log(">>> Getting questions from database\n");
+  constructor(databaseId: string) {
+    // Initialize notion client
+    this.client = new Client({ auth: notionKey });
+    this.databaseId = databaseId;
+    this.questions = [];
+  }
 
-// Query database for rows with Status "Done"
-const database = await (() => {
-  try {
-    return client.databases.query({
-      database_id: databaseId!,
-      filter: {
-        property: "Status",
-        status: {
-          equals: "Done",
-        },
-      },
+  // Queries database and returns a QueryDatabaseResponse object of rows
+  private async getDatabaseRows(filter: any) {
+    try {
+      const database = await this.client.databases.query({
+        database_id: this.databaseId!,
+        filter: filter,
+      });
+      return database;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Extracts question name from each row in QueryDatabaseResponse object
+  private getQuestionNames(database: QueryDatabaseResponse) {
+    // build questions array from database content
+    database?.results.forEach((row) => {
+      if (isFullPage(row) && "title" in row.properties.Name) {
+        this.questions.push(row.properties.Name.title[0].plain_text);
+      }
     });
-  } catch (error) {
-    console.log(error);
   }
-})();
+}
 
-// array to store questions with Status "Done" (completed questions)
-const questions: string[] = [];
-
-// build questions array from database content
-database?.results.forEach((row) => {
-  if (isFullPage(row) && "title" in row.properties.Name) {
-    questions.push(row.properties.Name.title[0].plain_text);
-  }
-});
-
-export default questions;
+// // Query database for rows with Status "Done"
+// const database = () => {
+//   try {
+//     return client.databases.query({
+//       database_id: databaseId!,
+//       filter: {
+//         property: "Status",
+//         status: {
+//           equals: "Done",
+//         },
+//       },
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
