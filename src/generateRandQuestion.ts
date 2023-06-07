7d1d2c93338fc4bfec624f2083bf3582f3763e1a
Bilal Ali <bilalal495@outlook.com>
2023-06-06 21:29:57 -0400

    Refactor code to generate random question into function

--- src/generateRandQuestion.ts
+++ src/generateRandQuestion.ts
@@ -1,8 +1,6 @@
-import questions from "./getQuestions.js";
-
 // number of questions acts as boundaries for random number generation
-const max = questions.length;
-const randomInt = Math.floor(Math.random() * max);
-const randQuestion = questions[randomInt];
-
-export default randQuestion;
+export const generateRandQuestion = (questions: string[]): string => {
+  const max = questions.length;
+  const randomInt = Math.floor(Math.random() * max);
+  return questions[randomInt];
+};
