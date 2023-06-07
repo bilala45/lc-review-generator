c6d2b0f7b8bc06d0324a075352fa75b83255fcf6
Bilal Ali <bilalal495@outlook.com>
2023-06-07 09:55:23 -0400

    Add function to generate Notion compliant date string with past date offset

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,46 @@
-// ! switch this from today's date to 3 days ago
-const TODAY = new Date().toISOString();
+// Returns Notion-compliant date string adjusted for date offset from today
+// dateOffset is the number of days in the PAST
+// limitations: only works on offsets that span the number of days in a month (0 - ~31)
+const computeDate = (dateOffset: number): string => {
+  const todayDate = new Date();
+
+  // calculate date, month, and year using offset
+  let dateNum = todayDate.getDate();
+  let monthNum = todayDate.getMonth();
+  let yearNum = todayDate.getFullYear();
+
+  if (dateNum - dateOffset <= 0) {
+    // check if previous month is December (year must change too in this case)
+    if (monthNum - 1 < 0) {
+      yearNum -= 1; // set year to previous year
+      // note that months are 0-indexed but since we subtract one to get the previous month, this will account for that
+      monthNum = 12; // set month to last month (december)
+    }
+
+    // set month to prevous month
+    monthNum -= 1;
+    // get number of days in previous month
+    const daysInPrevMonth = new Date(yearNum, monthNum, 0).getDate();
+    dateNum = daysInPrevMonth + (dateNum - dateOffset + 1);
+  } else {
+    dateNum -= dateOffset;
+  }
+
+  // append zero to date string if before 10th
+  let date: string = `${dateNum}`;
+  if (dateNum < 10) {
+    date = 0 + date;
+  }
+
+  // append zero to month if before October
+  // months are 0-indexed (add 1 to get 1-indexed month as string)
+  let month = `${monthNum + 1}`;
+  if (monthNum < 10) {
+    month = 0 + month;
+  }
+
+  return `${yearNum}-${month}-${date}`;
+};
 
 // filter for question database
 export const QUESTION_FILTER = {
@@ -13,7 +54,7 @@ export const QUESTION_FILTER = {
     {
       property: "Date Completed",
       date: {
-        before: TODAY,
+        before: computeDate(5),
       },
     },
   ],
@@ -23,7 +64,7 @@ export const QUESTION_FILTER = {
 export const REVIEW_FILTER = {
   property: "Date",
   date: {
-    before: TODAY,
+    after: computeDate(5),
   },
 };
 
@@ -31,6 +72,6 @@ export const REVIEW_FILTER = {
 export const TODAY_FILTER = {
   property: "Date",
   date: {
-    equals: TODAY,
+    equals: computeDate(0),
   },
 };
